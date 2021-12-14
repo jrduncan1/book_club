d14b0a2b685fd8f67ce11b087a78360058641c3b
jrduncan1 <dunc5201@gmail.com>
2021-12-14 14:54:56 -0700

    user model completed

---
+++ src/main/java/com/jduncan/bookclub/models/User.java
@@ -0,0 +1,120 @@
+package com.jduncan.bookclub.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+    // **** PRIMARY KEY ****
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    // **** MEMBER VARIABLES ****
+    
+    @NotEmpty(message="Name is required!")
+    @Size(min=2, message="Name must be at least 2 characters.")
+    private String name;
+    
+    @NotEmpty(message="Email is required!")
+    @Email(message="Please enter a valid email.")
+    private String email;
+    
+    @NotEmpty(message="Password is required!")
+    @Size(min=8, max=128, message="Password must be between {min} and {max} characters.")
+    private String password;
+    
+    @Transient
+    @NotEmpty(message="Passwords must match.")
+    @Size(min=8, max=128, message="Passwords must match.")
+    private String confirmPassword;
+    
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    // **** CONSTRUCTORS ****
+    
+    public User() {
+    	
+    }
+
+	public User(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	// **** GETTERS & SETTERS ****
+    
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+}
